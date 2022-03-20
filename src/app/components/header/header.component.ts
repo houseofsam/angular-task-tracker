660d6d86317904cac9a6c2fa489360b8af7ed478
houseofsam <samxsaid@gmail.com>
2022-03-20 13:43:58 -0400

    Implement Add button UI toggle using UIService
    to-do: hide form

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -7,12 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   title: string = 'Task Tracker';
-  constructor() { }
+  showAddTask: boolean;
+  subscription: Subscription;
+
+  // to use service, must add it to constructor ..why?
+  constructor(private uiService: UiService) {
+    // watch the subscription that we added in
+    this.subscription = this.uiService.onToggle().subscribe(value => this.showAddTask = value);
+    // watcher the onToggle function in uiService
+   }
 
   ngOnInit(): void {
   }
 
   toggleAddTask() {
-    console.log('toggle');
+    this.uiService.toggleAddTask();
   }
 }
